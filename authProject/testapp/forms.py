cb9dc58b913b5c409900d0ecea6a3bcbb98b9ca1
Sajjad Ali <yoursajju61@gmail.com>
2026-06-09 17:29:37 +0530

    feat: introduce SignUpForm with secure password widget masking

--- authProject/testapp/forms.py
+++ authProject/testapp/forms.py
@@ -2,7 +2,13 @@ from django import forms
 from django.contrib.auth.models import User
 
 class SignUpForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter Password'}))
+    
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ['username', 'password'] # Jo fields aapko chahiye
         
+        # Username field me bhi bootstrap class jodne ke liye widgets block:
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Username'}),
+        }
